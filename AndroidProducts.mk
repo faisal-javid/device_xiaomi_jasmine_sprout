b408019175f9cbc26f54d309fc1dd0388b75a482
Faisal Javid <83747891+faisal-javid@users.noreply.github.com>
2022-01-02 02:41:10 +0530

    fix typo

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -10,6 +10,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aosp_jasmine_sprout.mk
 
 COMMON_LUNCH_CHOICES := \
-    asop_jasmine_sprout-user \
+    aosp_jasmine_sprout-user \
     aosp_jasmine_sprout-userdebug \
     aosp_jasmine_sprout-eng
